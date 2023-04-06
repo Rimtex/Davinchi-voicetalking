11b194e8a0583562928aa5c0556edcfbf2151c81
Rimtex <Raider@ukr.net>
2023-04-07 02:38:46 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,12 +1,24 @@
-openaiapikeyset = "token"  # обязательно вставьте свой ключ !!!  https://platform.openai.com/account/api-keys
-modelset = Model(r"vosk-model-small-ru-0.22")  # будет ошибка если запускать из консоли. !!
-# modelset = r"F:\myprojects\Davinchi-voicetalking-main\vosk-model-small-ru-0.22"  # с вашим полным адресом должно работать
-adresopenfilesset = 'C:\\Users\\Public\\Desktop\\'  # адрес папки для команд открытия ! #
-# 'C:\\Users\\Usersname\\Desktop\\'
-Voiceset = "Microsoft Irina Desktop"  # Microsoft Pavel Mobile  файлы реестра в папке -  need install for pavel voice
-speaksetmin = 4  # скорость озвучки по-умолчанию
-speaksetmax = 10  # регулируется пропорционально количеству символов запроса:   0 - 700  =  speaksetmin - speaksetmax
-speakVolumeset = 100  # громкость голоса
-engineset = 'text-davinci-003'  # моделька для одиночного вызова  # https://platform.openai.com/docs/models/gpt-4
-discretset = 44100  # частота дискретизации должна быть такой же, как и в системе
-# звуки > запись > микрофон > свойства > дополнительно > выставляем также
+openaiapikeyset = "token"                           # обязательно вставьте свой api-key !!! 
+modelset = r"vosk-model-small-ru-0.22"              # будет ошибка если запускать из консоли. !! с полным адресом должно работать 
+           
+adresopenfilesset = 'C:\\Users\\Public\\Desktop\\'  # адрес папки для команд открытия !   
+
+Voiceset = "Microsoft Irina Desktop"                # Microsoft Pavel Mobile  файлы реестра в папке -  need install for pavel voice
+speaksetmin = 4                                     # скорость озвучки по-умолчанию     
+speaksetmax = 10                                    # максимальная скорость:   0 - 700 символов =  speaksetmin - speaksetmax
+speakVolumeset = 100                                # громкость голоса
+engineset = 'text-davinci-003'                      # моделька для одиночного вызова  
+discretset = 44100                                  # частота дискретизации должна быть такой же, как и в системе
+                                                    # звуки > запись > микрофон > свойства > дополнительно > выставляем также
+
+roleplayeng = "You are a helpful assistant."                       # роль для gpt-3.5-turbo с переводом
+roleplayrus = "Ты веселая девушка очень любишь смешить людей"      # роль для gpt-3.5-turbo без перевода
+
+  
+  
+https://platform.openai.com/account/api-keys
+https://platform.openai.com/docs/models/gpt-4
+
+  
+# пример полного адреса  
+# modelset = r"F:\myprojects\Davinchi-voicetalking-main\vosk-model-small-ru-0.22" 
